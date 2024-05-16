b7d238c5e66587566ea613e7c69599c4b0b5ac9b
raz2672 <123548343+Squeezy5448@users.noreply.github.com>
2024-05-16 18:54:39 +0800

    Update Codes.lua

--- Codes.lua
+++ Codes.lua
@@ -2,14 +2,16 @@
 	"StopYapping": {
 		"Code": "StopYapping",
 		"Active": true,
+		"Duration": 4,
 		"Reward": 3,
 		"RewardType": "Spins"
 	},
-
+	
 	"10stock": {
 		"Code": "10stock",
 		"Active": true,
-		"Reward": 17322476811, -- id
+		"Duration": 2,
+		"Reward": 17322476811,
 		"RewardType": "UGC"
 	}
 }
