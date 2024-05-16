9c970db4afad52bd56d93c5f4a2c96dac68fe323
raz2672 <123548343+Squeezy5448@users.noreply.github.com>
2024-05-16 17:40:41 +0800

    Create Yes.lua

---
+++ Yes.lua
@@ -0,0 +1,12 @@
+{
+	"StopYapping": {
+		"Code": "StopYapping",
+		"Reward": 3,
+    "RewardType": "Spins",
+	},
+  "10stock": {
+		"Code": "10stock",
+		"Reward": 17322476811,
+    "RewardType": "UGC",
+	},
+}
