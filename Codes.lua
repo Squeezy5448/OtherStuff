163bebce47e496946c40ee4a98ad90b83752641e
raz2672 <123548343+Squeezy5448@users.noreply.github.com>
2024-05-16 18:05:24 +0800

    Create Codes.lua

---
+++ Codes.lua
@@ -0,0 +1,13 @@
+{
+	"StopYapping": {
+		"Code": "StopYapping",
+		"Reward": 3,
+		"RewardType": "Spins"
+	},
+
+	"10stock": {
+		"Code": "10stock",
+		"Reward": 17322476811,
+		"RewardType": "UGC"
+	}
+}
